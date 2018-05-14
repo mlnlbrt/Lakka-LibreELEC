cf1b6b7d5d3d71d78c98a90f5495304175227ca1
Albert Malina <albert.malina@gmail.com>
2018-05-14 22:22:35 +0200

    Add separate Kernel for X11

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -91,6 +91,11 @@ case "$LINUX" in
     PKG_VERSION="4.14.5"
     PKG_URL="http://www.kernel.org/pub/linux/kernel/v4.x/$PKG_NAME-$PKG_VERSION.tar.xz"
     ;;
+  default-x11)
+    PKG_VERSION="4.11.12"
+    PKG_URL="http://www.kernel.org/pub/linux/kernel/v4.x/$PKG_NAME-$PKG_VERSION.tar.xz"
+    PKG_PATCH_DIRS="default-x11"
+    ;;
   *)
     PKG_VERSION="4.11.12"
     PKG_URL="http://www.kernel.org/pub/linux/kernel/v4.x/$PKG_NAME-$PKG_VERSION.tar.xz"
