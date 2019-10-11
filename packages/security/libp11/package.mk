84f463c02cc01a260e22bf726214da4dbb758e3f
Albert Malina <albert.malina@gmail.com>
2019-10-11 20:17:52 +0200

    libp11: add package

---
+++ packages/security/libp11/package.mk
@@ -0,0 +1,27 @@
+################################################################################
+#      This file is part of OpenELEC - http://www.openelec.tv
+#      Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
+#
+#  OpenELEC is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  OpenELEC is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with OpenELEC.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+PKG_NAME="libp11"
+PKG_VERSION="0.4.10"
+PKG_ARCH="any"
+PKG_URL="https://github.com/OpenSC/$PKG_NAME/releases/download/$PKG_NAME-$PKG_VERSION/$PKG_NAME-$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="openssl"
+PKG_SECTION="security"
+PKG_SHORTDESC="libp11: A library implementing a small layer on top of the PKCS11 API"
+PKG_IS_ADDON="no"
+PKG_AUTORECONF="no"
